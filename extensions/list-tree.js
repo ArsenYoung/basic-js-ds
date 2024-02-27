efc66ff4b3ae48bb300b9baec0fc884cc20246fa
arsenyoung <molodoiars@gmail.com>
2024-02-27 16:35:21 +0400

    feat: implemented add, find, has methods for binary-search-tree

--- extensions/list-tree.js
+++ extensions/list-tree.js
@@ -4,6 +4,60 @@ class Node {
     this.left = null;
     this.right = null;
   }
+
+  add(data) {
+      if (data < this.data) {
+          this.addLeft(data);
+      } else {
+          this.addRight(data);
+      }
+  }
+
+  addLeft(data) {
+      if (this.left) {
+          return this.left.add(data)
+      }
+      this.left = new Node(data);
+  }
+
+  addRight(data) {
+      if (this.right) {
+          return this.right.add(data)
+      }
+      this.right = new Node(data);
+  }
+
+  has(data) {
+    if (this.data === data) {
+      return true;
+    }
+
+    if (data < this.data && this.left !== null) {
+        return this.left.has(data);
+    }
+
+    if (data > this.data && this.right !== null) {
+        return this.right.has(data);
+    }
+
+    return false;
+  }
+
+  find(data) {
+    if (data === this.data) {
+      return this;
+    }
+
+    if (data < this.data && this.left !== null) {
+      return this.left.find(data);
+    }
+
+    if (data > this.data && this.right !== null) {
+      return this.right.find(data);
+    }
+
+    return null;
+  }
 }
 
 module.exports = {
--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,54 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  rootNode
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootNode === undefined) {
+        return null;
+    }
+
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.rootNode) {
+        return this.rootNode.add(data);
+    }       
+    this.rootNode = new Node(data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.rootNode.data === data) {
+        return true;
+    }
+
+    if (data < this.rootNode.data) {
+        return this.rootNode.left.has(data);
+    }
+
+    if (data > this.rootNode.data) {
+        return this.rootNode.right.has(data);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (data === this.rootNode.data) {
+      return this.rootNode;
+    }
+
+    if (data < this.rootNode.data) {
+      return this.rootNode.left.find(data);
+    }
+
+    if (data > this.rootNode.data) {
+      return this.rootNode.right.find(data);
+    }
   }
 
   remove(/* data */) {
